d7acbbdb4da7f66ba035485e30dadb8556c7ccc6
Halvor Bjørstad <43417915+Riphiphip@users.noreply.github.com>
2023-03-21 15:24:02 +0100

    Split StateEdge type to make mapping from jlm easier

---
+++ include/RVSDG/RVSDGInterfaces.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#include "RVSDG/TypeInterfaces.h.inc"
--- include/RVSDG/RVSDGTypes.h
+++ include/RVSDG/RVSDGTypes.h
@@ -3,5 +3,7 @@
 #include <mlir/IR/TypeSupport.h>
 #include <mlir/IR/Builders.h>
 
+#include "RVSDGInterfaces.h"
+
 #define GET_TYPEDEF_CLASSES
 #include "RVSDG/Types.h.inc"
